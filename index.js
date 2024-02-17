23d23763519feab4d797930760655656dcf58e00
Gpocas <gpocas01@gmail.com>
2024-02-16 21:15:40 -0300

    criando loop e melhorias gerais

--- index.js
+++ index.js
@@ -3,32 +3,36 @@ import puppeteer from "puppeteer";
 import { readFile, writeFile } from "node:fs";
 import { parse } from "node-html-parser";
 
-var last_ep;
+// Função para verificar e notificar sobre novos episódios
+async function checkForNewEpisodes() {
+  var last_ep;
 
-readFile("last_ep.txt", "utf8", function (err, data) {
-  last_ep = parseInt(data);
-});
+  // Lê o número do último episódio do arquivo
+  readFile("last_ep.txt", "utf8", function (err, data) {
+    if (err) {
+      console.error("Erro ao ler arquivo: ", err);
+      return;
+    }
+    last_ep = parseInt(data);
+  });
 
-(async () => {
-  // Launch the browser and open a new blank page
+  // Inicia o navegador
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  const bot = new Telegraf(process.env.BOT_TOKEN);
 
-  // Navigate the page to a URL
+  // Acessa a página dos episódios
   await page.goto("https://animeshouse.net/");
-
   await page.setViewport({ width: 1920, height: 1080 });
 
+  // Extrai informações sobre os episódios
   let html_contents = await page.content();
   let root = parse(html_contents);
-
-  await browser.close();
-
   let cards = root.querySelectorAll(".animation-2.items article .data");
-
   let episodes = [];
-  var url_final
+  let url_final;
+
+
+  // Verifica cada episódio encontrado
   for (let card of cards) {
     let name = card.querySelector("h3 a").innerText;
     let url = card.querySelector("h3 a").getAttribute("href");
@@ -38,23 +42,44 @@ readFile("last_ep.txt", "utf8", function (err, data) {
       .trim();
 
     if (name.trim() == "Solo Leveling") {
-      url_final = url
+      url_final = url;
       episodes.push(ep);
     }
   }
 
+
+  if (episodes.length === 0) {
+    console.log('sem novos episodios')
+    await browser.close();
+    return
+  } 
+  // Notifica sobre novos episódios e atualiza o arquivo
   for (let current_ep of episodes) {
     if (parseInt(current_ep) > last_ep) {
       bot.telegram.sendMessage(
         process.env.CHAT_ID,
         `Hello, New Episode Solo Leveling:
 
-Solo Leveling Episode ${current_ep}:
-${url_final}`
+        Solo Leveling Episode ${current_ep}:
+        ${url_final}`
       );
       writeFile("last_ep.txt", current_ep, (err) => {
         if (err) console.log(err);
       });
     }
+    else {
+      console.log('sem novos episodios')
+      await browser.close();
+      return
+    }
   }
-})();
+  
+  // Fecha o navegador
+  await browser.close();
+}
+
+// Cria um loop para verificar episódios a cada 5 minutos
+setInterval(checkForNewEpisodes, 5 * 60 * 1000);
+
+// Cria o bot do Telegram
+const bot = new Telegraf(process.env.BOT_TOKEN);
